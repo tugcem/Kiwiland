ba34c5ce95045af966c2205d74bd14bfb64790df
tuğçem yalçın <tugcemyalcin@gmail.com>
2015-10-08 13:00:00 +0300

    Writes down outputs of given examples.

--- railroad_runner.rb
+++ railroad_runner.rb
@@ -3,21 +3,15 @@ require './railroad.rb'
 class RailroadRunner
 
   railroad = Railroad.new(ARGF.filename)
-  10.times do |count|
-    puts "Output##{count}:" + send("call_#{count}")
-  end
-  
-  def call_1
-    get_distance_of ['A', 'B', 'C']
-  end
-  # puts "Output#1:#{railroad.get_distance_of ['A', 'B', 'C']}"
-  # puts "Output#2:#{railroad.get_distance_of ['A', 'D'] }"
-  # puts "Output#3:#{railroad.get_distance_of ['A', 'D', 'C']}"
-  # puts "Output#4:#{railroad.get_distance_of ['A', 'E', 'B', 'C', 'D']}"
-  # puts "Output#5:#{railroad.get_distance_of ['A', 'E', 'D']}"
-  # puts "Output#6:#{railroad.number_of_different_routes 'C', 'C', max_stop: 3}"
-  # puts "Output#7:#{railroad.number_of_different_routes 'A', 'C', exact_stop: 4}"
-  # puts "Output#8:#{railroad.distance_of_shortest_path 'A', 'C'}"
-  # puts "Output#9:#{railroad.distance_of_shortest_path 'B', 'B'}"
-  # puts "Output#10:#{railroad.number_of_different_routes 'C', 'C', max_distance: 30}"
+
+  puts "Output#1:#{railroad.get_distance_of ['A', 'B', 'C']}"
+  puts "Output#2:#{railroad.get_distance_of ['A', 'D'] }"
+  puts "Output#3:#{railroad.get_distance_of ['A', 'D', 'C']}"
+  puts "Output#4:#{railroad.get_distance_of ['A', 'E', 'B', 'C', 'D']}"
+  puts "Output#5:#{railroad.get_distance_of ['A', 'E', 'D']}"
+  puts "Output#6:#{railroad.number_of_different_routes 'C', 'C', max_stop: 3}"
+  puts "Output#7:#{railroad.number_of_different_routes 'A', 'C', exact_stop: 4}"
+  puts "Output#8:#{railroad.distance_of_shortest_path 'A', 'C'}"
+  puts "Output#9:#{railroad.distance_of_shortest_path 'B', 'B'}"
+  puts "Output#10:#{railroad.number_of_different_routes 'C', 'C', max_distance: 30}"
 end
